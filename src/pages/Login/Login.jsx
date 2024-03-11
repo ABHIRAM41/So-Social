bbd9be23b3d6a6788308996e31112867f33a5b29
ABHIRAM41 <jabhiramrunn3@gmail.com>
2024-03-11 14:59:05 +0530

    connect

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
-    <div className="w-screen h-screen bg-green-100 flex items-center justify-center">
+    <div className="w-screen h-screen bg-green-100 flex items-center justify-center ">
       <div className="w-[70%] h-[70%] flex">
         <div className="flex flex-col justify-center" style={{ flex: 1 }}>
           <h1 className="font-extrabold text-3xl text-green-600">
-            Social Media
+            So Social
           </h1>
           <span className="text-lg font-semibold">Connect with friends</span>
         </div>
         <div className="flex flex-col justify-center" style={{ flex: 1 }}>
           <div className="bg-white h-[300px] p-[20px] rounded-md flex flex-col justify-between shadow-lg">
-            
             <input
               type="email"
               placeholder="email"
@@ -23,18 +22,26 @@ const Login = () => {
               placeholder="pasword"
               className="h-[50px] rounded-md border border-gray-200 text-lg p-[20px] focus:outline-none"
             />
-            <button className="h-[50px] rounded-lg bg-green-600 hover:bg-green-700 transition text-white text-lg font-bold">
+            <Link
+              to="/"
+              className="h-[50px] rounded-lg bg-green-600 hover:bg-green-700 transition flex items-center justify-center text-white text-lg font-bold"
+            >
               Login
-            </button>
-            <span className='text-center text-green-600 cursor-pointer'>Forgot password..?</span>
-            <button className="h-[50px] w-1/2 rounded-lg bg-blue-600 hover:bg-blue-700 transition text-white text-lg font-bold self-center">
+            </Link>
+            <span className="text-center text-green-600 cursor-pointer">
+              Forgot password..?
+            </span>
+            <Link
+              to={"/register"}
+              className="h-[50px] w-1/2 rounded-lg bg-blue-600 hover:bg-blue-700 transition flex items-center justify-center text-white text-lg font-bold self-center"
+            >
               Create a new Account
-            </button>
+            </Link>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Register = () => {
   return (
     <div className="w-screen h-screen bg-green-100 flex items-center justify-center">
       <div className="w-[70%] h-[70%] flex">
         <div className="flex flex-col justify-center" style={{ flex: 1 }}>
           <h1 className="font-extrabold text-3xl text-green-600">
-            Social Media
+            So Social
           </h1>
           <span className="text-lg font-semibold">Connect with friends</span>
         </div>
@@ -32,12 +32,18 @@ const Register = () => {
               placeholder="comfirm password"
               className="h-[50px] rounded-md border border-gray-200 text-lg p-[20px] focus:outline-none"
             />
-            <button className="h-[50px] rounded-lg bg-green-600 hover:bg-green-700 transition text-white text-lg font-bold">
+            <Link
+              to="/"
+              className="h-[50px] rounded-lg bg-green-600 hover:bg-green-700 transition flex items-center justify-center text-white text-lg font-bold"
+            >
               Sign Up
-            </button>
-            <button className="h-[50px] w-1/2 rounded-lg bg-blue-600 hover:bg-blue-700 transition text-white text-lg font-bold self-center">
+            </Link>
+            <Link
+              to={"/login"}
+              className="h-[50px] w-1/2 rounded-lg bg-blue-600 hover:bg-blue-700 transition flex items-center justify-center text-white text-lg font-bold self-center"
+            >
               Login Into Your Account
-            </button>
+            </Link>
           </div>
         </div>
       </div>
